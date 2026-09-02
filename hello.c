1df2a74bf4f45fbf80e437f6966d2fbd58ba6a44
Devan Mathis <d130m536@home.ku.edu>
2026-09-02 09:58:01 -0500

    Replace hello-world program w/ sample C program + fgets() + delete binary exe file

--- hello.c
+++ hello.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
-int main(){
+#include <string.h>
+
+int main()
+{
+/* old code
 	printf("Goodbye World... until next time :)\n");
+	return 0;
+*/
+	char str[100];
+	int j, len;
+	
+	printf("Enter a string: ");
+	fgets(str, 100, stdin);
+
+	len = strlen(str);
+	
+	for (j = 0; j < len; j++) {
+		if (str[j] >= 'a' && str[j] <= 'z') {
+			str[j] = str[j] - 'a' + 'A';
+		}
+	}
+	
+	printf("The capitalized string is: %s\n", str);
+	
+	// Reverse the string.
+	for (j = 0, len = strlen(str); j < len / 2; j++) {
+		char temp = str[j];
+		str[j] = str[len - 1 - j];
+		str[len - 1 - j] = temp;
+	}
+	
+	printf("The reversed string is: %s\n", str);
+	
+
 	return 0;
 }
